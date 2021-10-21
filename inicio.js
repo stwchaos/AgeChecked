08ff16749fd0108e985fad948adf7a9cb8799f45
stwchaos <giovannadev.pfl@gmail.com>
2021-10-21 17:26:52 -0300

    atualização

--- inicio.js
+++ inicio.js
@@ -18,18 +18,6 @@ function verificar(){
       //genero
       genero = 'homem'
       pron = 'um'
-      
-      //imagem
-      if (idade >= 0 && idade < 12){
-        gif.innerHTML= '<iframe src= https://tenor.com/embed.js href="https://tenor.com/view/she-ra-imp-hordak-entrapdak-gif-15650290"></a><a href="https://tenor.com/search/she+ra-gifs">She Ra GIFs</a></div> <script type="text/javascript></script>'
-
-      } else if (idade < 18){
-
-      } else if (idade < 65){
-
-      } else {
-
-      }
 
 
     } else if(fgen[1].checked){
@@ -37,33 +25,13 @@ function verificar(){
       genero = 'mulher'
       pron = 'uma'
 
-      //imagem
-      if (idade >= 0 && idade < 12){
-        
-
-      } else if (idade < 18){
-
-      } else if (idade < 65){
-
-      } else {
-
-      }
+     
 
     } else if (fgen[2].checked){
       //genero
       genero = 'NB'
       pron = 'ume'
 
-      //imagem
-      if (idade >= 0 && idade < 12){
-
-      } else if (idade < 18){
-
-      } else if (idade < 65){
-
-      } else {
-
-      }
 
     }
     
